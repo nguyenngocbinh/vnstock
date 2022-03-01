b77246c015a3dacdd66b7889dac024a7f60b26f1
Nguyen Ngoc Binh <nguyenngocbinhneu@gmail.com>
2022-03-01 18:39:53 +0700

    get list tickers

--- R/vndirect.R
+++ R/vndirect.R
@@ -1,14 +1,13 @@
-#' Request the VNDIRECT API
+#' REQUEST THE VNDIRECT API
 #' @param .ticker stock symbol
 #' @param .size length of historical data
 #' @return data.frame
-#' @importFrom httr GET content
 #' @importFrom magrittr extract2 `%>%`
+#' @importFrom dplyr bind_rows rename mutate
 #' @importFrom purrr map_dfr
-#' @importFrom dplyr bind_rows
-#' @example vnd_get_data('VCB', 1000)
+#' @example vnd_get_data('TPB', 1000)
 #' @export
-vnd_get_data <- function(.ticker = NULL, .size = 1000) {
+vnd_get_data <- function(.ticker = NULL, .size = 100) {
   
   if (is.null(.ticker)) {
     stop('.ticker is not set')
@@ -29,14 +28,47 @@ vnd_get_data <- function(.ticker = NULL, .size = 1000) {
     q = endpoint
   )
   
-  res <- httr::GET(base, query = params)
+  res <- httr::GET(base, query = params) %>% 
+    httr::content() %>% 
+    extract2('data')
   
-  df <- httr::content(res) %>% 
-    magrittr::extract2('data') %>% 
-    purrr::map_dfr(bind_rows) %>% 
-    dplyr::rename(ticker_name = code) %>% 
-    dplyr::mutate(date = as.Date(date))
-    
+  if(length(res) < 1) {
+    stop(paste('Do not have data. Try to correct ticker_name:', .ticker))
+  }
+  
+  
+  df <-  res %>% 
+    map_dfr(bind_rows) %>% 
+    rename(ticker_name = code) %>% 
+    mutate(date = as.Date(date))
+  
   
   df
 }
+
+
+#' GET DATA FROM MULTIPLE STOCKS
+#' @param .tickers list of stock symbols
+#' @param .size length of historical data
+#' @return data.frame
+#' @importFrom purrr map_dfr possibly
+#' @example vnd_get_list_data(c('TPB', 'VCB'))
+#' @export
+vnd_get_list_data <- function(.tickers = NULL, .size = 100) {
+  if (is.null(.tickers)) {
+    stop('.ticker is not set')
+  }
+  
+  if (.size <= 0) {
+    stop('.size must be > 0')
+  }
+  
+  df <- map_dfr(.tickers,
+                possibly(vnd_get_data, NULL),
+                .size = .size)
+  
+  return(df)
+}
+
+
+
